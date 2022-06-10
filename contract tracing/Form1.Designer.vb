d7f453b6dcb9bef09a85352242b9185cf650f5e4
steven <steven17@gmail.com>
2022-06-11 03:53:11 +0800

    added textbox buttons and code to streamwrite to textfile

---
+++ contract tracing/Form1.Designer.vb
@@ -0,0 +1,161 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.txtname = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtage = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtgender = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtaddress = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.txtcontact = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(23, 269)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(226, 46)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "submit"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'txtname
+        '
+        Me.txtname.Location = New System.Drawing.Point(75, 59)
+        Me.txtname.Name = "txtname"
+        Me.txtname.Size = New System.Drawing.Size(174, 23)
+        Me.txtname.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(23, 62)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(40, 15)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "name:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(23, 103)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(29, 15)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "age:"
+        '
+        'txtage
+        '
+        Me.txtage.Location = New System.Drawing.Point(75, 100)
+        Me.txtage.Name = "txtage"
+        Me.txtage.Size = New System.Drawing.Size(174, 23)
+        Me.txtage.TabIndex = 3
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(23, 149)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(47, 15)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "gender:"
+        '
+        'txtgender
+        '
+        Me.txtgender.Location = New System.Drawing.Point(75, 146)
+        Me.txtgender.Name = "txtgender"
+        Me.txtgender.Size = New System.Drawing.Size(174, 23)
+        Me.txtgender.TabIndex = 5
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(23, 190)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(50, 15)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "address:"
+        '
+        'txtaddress
+        '
+        Me.txtaddress.Location = New System.Drawing.Point(75, 187)
+        Me.txtaddress.Name = "txtaddress"
+        Me.txtaddress.Size = New System.Drawing.Size(174, 23)
+        Me.txtaddress.TabIndex = 7
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(23, 230)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(50, 15)
+        Me.Label5.TabIndex = 10
+        Me.Label5.Text = "contact:"
+        '
+        'txtcontact
+        '
+        Me.txtcontact.Location = New System.Drawing.Point(75, 227)
+        Me.txtcontact.Name = "txtcontact"
+        Me.txtcontact.Size = New System.Drawing.Size(174, 23)
+        Me.txtcontact.TabIndex = 9
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1431, 798)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.txtcontact)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.txtaddress)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txtgender)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtage)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtname)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents txtname As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtage As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtgender As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtaddress As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents txtcontact As TextBox
+End Class
---
+++ contract tracing/Form1.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim xd As System.IO.StreamWriter
+        xd = My.Computer.FileSystem.OpenTextFileWriter("cnt.txt", True)
+        xd.WriteLine("name:" + txtname.Text)
+        xd.WriteLine("age" + txtage.Text)
+        xd.WriteLine("gender" + txtgender.Text)
+        xd.WriteLine("address" + txtaddress.Text)
+        xd.WriteLine("contact" + txtcontact.Text)
+        xd.Close()
+        MessageBox.Show("datas has been added")
+
+    End Sub
+End Class
